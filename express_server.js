306ce31a968a658429386adb25f196bacd738c33
JIAQI13 <jiaqi11@ualberta.ca>
2021-05-11 03:55:28 +0000

    index page html added

--- express_server.js
+++ express_server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
+app.set("view engine", "ejs");
+
 const urlDatabase = {
     "b2xVn2": "http://www.lighthouselabs.ca",
     "9sm5xK": "http://www.google.com"
@@ -26,6 +28,16 @@ app.get("/set", (req, res) => {
 
 app.get("/fetch", (req, res) => {
     res.send(`a = ${a}`);
+});
+
+app.get("/urls", (req, res) => {
+    const templateVars = { urls: urlDatabase };
+    res.render("urls_index", templateVars);
+});
+
+app.get("/hello", (req, res) => {
+    const templateVars = { greeting: 'Hello World!' };
+    res.render("hello_world", templateVars);
 });
 
 app.listen(PORT, () => {
