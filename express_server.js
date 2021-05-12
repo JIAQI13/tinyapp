730a6e60e5e0dd70a2ea8ab8aeb6fe54e376a614
JIAQI13 <jiaqi11@ualberta.ca>
2021-05-12 00:06:29 +0000

    creat finished

--- express_server.js
+++ express_server.js
@@ -2,6 +2,7 @@ const PORT = 8080; // default port 8080
 const express = require("express");
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
+var crypto = require("crypto");
 
 const app = express();
 app.set("view engine", "ejs");
@@ -13,6 +14,22 @@ const urlDatabase = {
     "b2xVn2": "http://www.lighthouselabs.ca",
     "9sm5xK": "http://www.google.com"
 };
+
+/*
+utility function that generate random string for our url
+*/
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+}
+
+function generateRandomString() {
+    let result = '';
+    let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    for (var i = 0; i < 7; i++) {
+        result += characters[getRandomInt(characters.length)];
+    }
+    return result;
+}
 
 /*
 use middleware
@@ -39,6 +56,14 @@ app.get("/urls/new", (req, res) => {
     res.render("urls_new");
 });
 
+app.post("/urls", (req, res) => {
+    let newKey = generateRandomString();
+    console.log(req.body);
+    urlDatabase[newKey] = 'http://' + String(req.body.longURL);
+    res.redirect('/urls');
+});
+
+
 /*
 delete
 */
@@ -48,7 +73,9 @@ app.post("/urls/:shortURL/delete", (req, res) => {
     res.redirect("/urls");
 });
 
-
+/*
+read
+*/
 app.get("/urls/:shortURL", (req, res) => {
     const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
     res.render("urls_show", templateVars);
@@ -61,6 +88,4 @@ app.get("/u/:shortURL", (req, res) => {
 
 
 
-app.listen(PORT, () => {
-    console.log(`Example app listening on port ${PORT}!`);
-});
+app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`));
